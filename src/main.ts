56f4a5d529d6ef835093f43bdf205c57db935880
Kelly <kelly@MacBook-Pro.local>
2025-02-06 14:13:32 +0700

    feat: 36397 - add swagger for user api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('HMD API Documentation')
+    .setDescription('Play with the API')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http' }, 'user')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
