d9b5653d87d1f955a636f3d95f5863e0a8829ab7
Kelly <kelly@MacBook-Pro.local>
2025-02-11 16:17:49 +0700

    feat: 36397 - update app.module, user service

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,17 +1,13 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from './users.service';
+import { SwaggerTags } from '../../common/constants/enums/swagger-tags.enum';
 
 @Controller('users')
-@ApiTags('USERS')
+@ApiTags(SwaggerTags.USERS)
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Get()
   getListOfUsers() {
